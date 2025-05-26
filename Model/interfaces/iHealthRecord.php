2e992266afdd7fc07f55085fb5bf2542844ea131
Im2rnado <yassin.bedier@gmail.com>
2025-05-26 23:38:00 +0300

    updates

--- Model/interfaces/iHealthRecord.php
+++ Model/interfaces/iHealthRecord.php
@@ -1,12 +1,19 @@
 <?php
-// Model/interfaces/IHealthRecord.php
+// Model/interfaces/iHealthRecord.php
 namespace Model\interfaces;
 
-interface IHealthRecord
-{
-    public function addRecord($data);
-    public function getRecord($id);
-    public function updateRecord($id, $data);
-    public function getDescription();
+interface iHealthRecord {
+    /**
+     * Get health record data
+     * 
+     * @return array Record data
+     */
     public function getData();
+    
+    /**
+     * Get health record description
+     * 
+     * @return string Record description
+     */
+    public function getDescription();
 }
